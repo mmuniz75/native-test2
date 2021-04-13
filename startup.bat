f0b1d7d5c2e3dfb8530b8fc4e41dc5af27478178
Marcelo Muniz Silva <muniz_marcelo@hotmail.com>
2021-04-13 12:04:59 -0300

    Delete startup.bat

--- startup.bat
+++
@@ -1,4 +0,0 @@
-set DATABASE_URL=postgres://postgres:postgres@localhost:5432/mktmsg
-set APPLICATION_WEBSITE=http://localhost:4200
-set AUTHENTICATOR_SERVER=https://gz31jsakaj.execute-api.sa-east-1.amazonaws.com/v1
-java -jar ./target/security-mktmsg-1.0.0-SNAPSHOT.jar
