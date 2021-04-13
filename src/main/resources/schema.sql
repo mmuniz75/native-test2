5e1e1f248b14776525ca8a850343dbf0a212e665
mmuniz75 <xrPLckq7tjHX2WVDKvZK>
2021-04-13 10:03:19 -0300

    add login with password encoder

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -20,7 +20,7 @@ CREATE TABLE _user (
 
 insert into product (id,name,max_users) values (382833,'Whatsapp Silver',10);
 insert into product (id,name,max_users) values (382834,'Whatsapp Gold',100);
-INSERT INTO _user (id,name,email,password,cpf,mobile,creation_date,validation_date,last_login,product_id) VALUES (1, 'Marcelo','mmunizs1975@gmail.com','{bcrypt}$2a$10$09VX30H6xCEpv4RrHDMa.uxcF0pJ0uARaqilwPmJPegwxngUMeNk2','45756392068','19999908070','2020-05-12 08:18:36.174','2020-06-12 08:20:26.493','2020-05-12 10:39:42.815',382833);
+INSERT INTO _user (id,name,email,password,cpf,mobile,creation_date,validation_date,last_login,product_id) VALUES (1, 'Marcelo','mmunizs1975@gmail.com','{bcrypt}$2a$10$09VX30H6xCEpv4RrHDMa.uxcF0pJ0uARaqilwPmJPegwxngUMeNk2','45756392068','19999908070','2020-05-12 08:18:36.174','2021-06-12 08:20:26.493','2020-05-12 10:39:42.815',382833);
 INSERT INTO _user (id,name,email,password,cpf,mobile,creation_date,validation_date,last_login,product_id) VALUES (2, 'Without password','mmunizs-forcepass@gmail.com','{bcrypt}$2a$10$09VX30H6xCEpv4RrHDMa.uxcF0pJ0uARaqilwPmJPegwxngUMeNk2','45756392067','18999908070','2020-05-12 08:18:36.174','2020-06-12 08:20:26.493','2020-05-12 10:39:42.815',382833);
 
 
